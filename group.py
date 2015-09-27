65c8309b8967482431fe3dc7839582ebbfa6051c
felix174 <felix174@mail.ru>
2015-09-27 18:12:22 +0300

    Home_work_3_OUT

--- group.py
+++ group.py
@@ -6,3 +6,27 @@ class Group:
         self.name = name
         self.header = header
         self.footer = footer
+
+class Group_contact:
+    # create parameters for contact
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax,
+                 form_01, byear, form_02, form_03, form_04, ayear, address2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.form_01 = form_01
+        self.byear = byear
+        self.form_02 = form_02
+        self.form_03 = form_03
+        self.form_04 = form_04
+        self.ayear = ayear
+        self.address2 = address2
+        self.notes = notes
--- sel.py
+++ sel.py
@@ -3,6 +3,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
 from group import Group
+from group import Group_contact
 
 def is_alert_present(wd):
     try:
@@ -16,14 +17,20 @@ class type_04(unittest.TestCase):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
 
+    # add contact
     def test_second(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.add_new_contact(wd)
+        self.add_new_contact(wd, Group_contact(firstname="Dmytro", middlename="Victorovuch", lastname="Rudenko",
+                                               nickname="felix174", title="web", company="promo", address="chabanu",
+                                               home="012345678", mobile="7777777777", work="555555555", fax="154541455",
+                                               form_01="8", byear="1989", form_02="10", form_03="12", form_04="3",
+                                               ayear="2013", address2="none_2", notes="my secount work"))
         self.return_to_contact_page(wd)
         self.logout(wd)
 
+    # add group
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
@@ -33,6 +40,7 @@ class type_04(unittest.TestCase):
         self.return_to_group_page(wd)
         self.logout(wd)
 
+    # add group with number
     def test_add_number_group(self):
         wd = self.wd
         self.open_home_page(wd)
@@ -42,72 +50,67 @@ class type_04(unittest.TestCase):
         self.return_to_group_page(wd)
         self.logout(wd)
 
-
     def return_to_contact_page(self, wd):
-        # return to contact page
         wd.find_element_by_link_text("home").click()
 
-    def add_new_contact(self, wd, firstname="Dmytro", middlename="Victorovuch", lastname="Rudenko", nickname="felix174",
-                        title="web", company="promo", address="chabanu", home="012345678", mobile="7777777777",
-                        work="555555555", fax="154541455", form_01="8", byear="1989", form_02="10", form_03="12", form_04="3",
-                        ayear="2013", address2="none_2", notes="my secount work"):
+    def add_new_contact(self, wd, group):
         # add new contact
         wd.find_element_by_link_text("add new").click()
         # fill contact firm
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(group.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(group.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(group.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(group.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(group.address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(home)
+        wd.find_element_by_name("home").send_keys(group.home)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobile)
+        wd.find_element_by_name("mobile").send_keys(group.mobile)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(work)
+        wd.find_element_by_name("work").send_keys(group.work)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(fax)
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[%s]" % form_01).is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[%s]" % form_01).click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[%s]" % form_02).is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[%s]" % form_02).click()
+        wd.find_element_by_name("fax").send_keys(group.fax)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[%s]" % group.form_01).is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[%s]" % group.form_01).click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[%s]" % group.form_02).is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[%s]" % group.form_02).click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(byear)
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[%s]" % form_03).is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[%s]" % form_03).click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[%s]" % form_04).is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[%s]" % form_04).click()
+        wd.find_element_by_name("byear").send_keys(group.byear)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[%s]" % group.form_03).is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[%s]" % group.form_03).click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[%s]" % group.form_04).is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[%s]" % group.form_04).click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(ayear)
+        wd.find_element_by_name("ayear").send_keys(group.ayear)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(address2)
+        wd.find_element_by_name("address2").send_keys(group.address2)
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(notes)
+        wd.find_element_by_name("notes").send_keys(group.notes)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
