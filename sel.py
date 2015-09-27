de3d7e4c5d36a7d381941adf8a0f981db58aaf4f
felix174 <felix174@mail.ru>
2015-09-27 16:12:17 +0300

    home_work_3_work_02

--- sel.py
+++ sel.py
@@ -47,64 +47,67 @@ class type_04(unittest.TestCase):
         # return to contact page
         wd.find_element_by_link_text("home").click()
 
-    def add_new_contact(self, wd):
+    def add_new_contact(self, wd, firstname="Dmytro", middlename="Victorovuch", lastname="Rudenko", nickname="felix174",
+                        title="web", company="promo", address="chabanu", home="012345678", mobile="7777777777",
+                        work="555555555", fax="154541455", form_01="8", byear="1989", form_02="10", form_03="12", form_04="3",
+                        ayear="2013", address2="none_2", notes="my secount work"):
         # add new contact
         wd.find_element_by_link_text("add new").click()
         # fill contact firm
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Dmytro")
+        wd.find_element_by_name("firstname").send_keys(firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Victorovuch")
+        wd.find_element_by_name("middlename").send_keys(middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Rudenko")
+        wd.find_element_by_name("lastname").send_keys(lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("felix174")
+        wd.find_element_by_name("nickname").send_keys(nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("web")
+        wd.find_element_by_name("title").send_keys(title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("promo")
+        wd.find_element_by_name("company").send_keys(company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("chabanu")
+        wd.find_element_by_name("address").send_keys(address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("012345678")
+        wd.find_element_by_name("home").send_keys(home)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("7777777777")
+        wd.find_element_by_name("mobile").send_keys(mobile)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("555555555")
+        wd.find_element_by_name("work").send_keys(work)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("154541455")
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[8]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[8]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[10]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[10]").click()
+        wd.find_element_by_name("fax").send_keys(fax)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[%s]" % form_01).is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[%s]" % form_01).click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[%s]" % form_02).is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[%s]" % form_02).click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1989")
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[12]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[12]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[3]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[3]").click()
+        wd.find_element_by_name("byear").send_keys(byear)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[%s]" % form_03).is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[%s]" % form_03).click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[%s]" % form_04).is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[%s]" % form_04).click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys("2013")
+        wd.find_element_by_name("ayear").send_keys(ayear)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("none_2")
+        wd.find_element_by_name("address2").send_keys(address2)
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("my secount work")
+        wd.find_element_by_name("notes").send_keys(notes)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
